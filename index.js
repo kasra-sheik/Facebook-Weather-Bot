14494194f396feadaafcaee5025823a4eab9a114
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-06-30 13:06:37 -0700

    updating messages

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 app.set('port', (process.env.PORT || 5000))
 
