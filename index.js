ca456515d4eda46479ef93b2d4e77ead854a8320
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-03 16:56:48 -0700

    commit

--- index.js
+++ index.js
@@ -152,34 +152,7 @@ function sendTextMessage(sender, text) {
     })
 }
 
-function sendVideo(sender) {
-    messageData = {
-        "message": {
-            "attachment": {
-                "type":"video",, 
-                "payload":{
-                    "url":"https://www.youtube.com/watch?v=fus357b19io"
-                }
-            }
-        }
 
-    }
-  request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
 
 // Send an test message back as two cards.
 
