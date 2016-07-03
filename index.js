d399d83064600261207138d25045fe7baa60f8a8
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-03 12:40:21 -0700

    commit

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ app.post('/webhook/', function (req, res) {
 
             }
             else if(text == 'generate') {
-                $.ajax( {
-                    type: 'GET',
-                    url: 'http://api.ipinfodb.com/v3/ip-city/?key=57a270e806c9470043d95781a3fcef13a6b86fa75c05ffd6908308d0dd1e4143&ip=74.125.45.100&format=json',
-                    success: function(data) {
-                        sendTextMessage(sender, "are we here?");
-                    }
+                // $.ajax( {
+                //     type: 'GET',
+                //     url: 'http://api.ipinfodb.com/v3/ip-city/?key=57a270e806c9470043d95781a3fcef13a6b86fa75c05ffd6908308d0dd1e4143&ip=74.125.45.100&format=json',
+                //     success: function(data) {
+                //         sendTextMessage(sender, "are we here?");
+                //     }
 
-                });
+                // });
             }
 
             else if(text == 'tell me a joke!!!') {
