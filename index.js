68fee3f1a5af7115b3458134f0a0376e09bd651d
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-03 13:24:04 -0700

    commit

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook/', function (req, res) {
                     // Get the response body
 
                     var rep = response.getBody();
-                    var repText = "The IP you requested is from " + rep.cityName + ", " + rep.regionName + rep.countryName + ", " + rep.zipCode
+                    var repText = "The IP you requested is from " + rep.cityName + ", " + rep.regionName + ", " + rep.zipCode
 
 
                     sendTextMessage(sender, repText)   
