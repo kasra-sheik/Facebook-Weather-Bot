414a85bfe1beba239234b8ed21f6c1829eed6bb5
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-05 12:13:31 -0700

    commit

--- index.js
+++ index.js
@@ -428,6 +428,22 @@ function testReceipt(sender) {
 
 
     }
+      request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 
 
