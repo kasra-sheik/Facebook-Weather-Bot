7d5200191ebc7c5f5e9e2253c91e9a85844fa65b
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-05 17:00:17 -0700

    commit

--- index.js
+++ index.js
@@ -244,26 +244,55 @@ function forecastBuilder(sender, forecastObject) {
 
 
 
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "yo"
-                }]
+
+    // messageData = {
+    //     "attachment": {
+    //         "type": "template",
+    //         "payload": {
+    //             "template_type": "generic",
+    //             "elements": [{
+    //                 "title": "yo"
+    //             }]
 
 
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
      request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
         method: 'POST',
         json: {
             recipient: {id:sender},
-            message: messageData,
+            message: 
+ "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+          {
+            "title":"Welcome to Peter\'s Hats",
+            "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
+            "subtitle":"We\'ve got the right hat for everyone.",
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+                "title":"View Website"
+              },
+              {
+                "type":"postback",
+                "title":"Start Chatting",
+                "payload":"USER_DEFINED_PAYLOAD"
+              }              
+            ]
+          }
+        ]
+      }
+    }
+  }
+,
         }
     }, function(error, response, body) {
         if (error) {
