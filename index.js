6b3ba1ecc67558a343dfb27a08ce5b221795e411
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-05 22:57:48 -0700

    commit

--- index.js
+++ index.js
@@ -209,6 +209,7 @@ app.post('/webhook/', function (req, res) {
                 if(postback_text == "\"Macy's Red Dress\"" || postback_text == "\"Bloomingdale's Red Dress\"" || postback_text == "\"Sak's Fifth Avenue Dress\"" ) {
                     sendTextMessage(sender, "I added " + postback_text + " to your cart");
                     cart_items.push("test")
+                    sendTextMessage(sender, cart_items.length)
 
                 }
                 else if(event.postback == "\"cart_payload\"") {
