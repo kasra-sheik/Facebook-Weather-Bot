a2ecbcf9128baece9e295b66c6535b9eb63b78e5
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-07 16:06:09 -0700

    message

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post('/webhook/', function (req, res) {
 
 
                     if(rep.entities.length > 0) {
-                        if(intent in rep.entities) {
+                        if("intent" in rep.entities) {
                             sendTextMessage(sender, "here is a test")
                         }
 
