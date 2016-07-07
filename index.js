fdc3d5256d0b24a17475a79ba0ffc0627262c2f8
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-07 15:35:10 -0700

    message

--- index.js
+++ index.js
@@ -55,24 +55,24 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             text = event.message.text
 
-            // var urlTestText = text
-            // for(i = 0; i < urlTestText.length; i++) {
+            var urlTestText = text
+            for(i = 0; i < urlTestText.length; i++) {
 
-            //     if(text[i] == " ") {
-            //        urlTestText = urlTestText.replaceAt(i,"+")
-            //     }
-            // }
-            // // var witURL = "https://api.wit.ai/message?v=20160707&q=" + urlTestText + "&access_token=P3FOKDQK5E3MKIPS7OH6ZBJ6DYCPSAQN"
+                if(text[i] == " ") {
+                   urlTestText = urlTestText.replaceAt(i,"+")
+                }
+            }
+          var witURL = "https://api.wit.ai/message?v=20160707&q=" + urlTestText + "&access_token=P3FOKDQK5E3MKIPS7OH6ZBJ6DYCPSAQN"
             
 
-            // //    requestify.get(URL).then(function(response) {
-            // //         // Get the response body
-            // //         var rep = response.getBody();
-            // //         var repText = rep.entities.location[0]
-            // //         sendTextMessage(sender, repText.value)
-            // //         sendTextMessage(sender, "we are here!")
+               requestify.get(witURL).then(function(response) {
+                    // Get the response body
+                    var rep = response.getBody();
+                    var repText = rep.entities.location[0]
+                    sendTextMessage(sender, repText.value)
+                    sendTextMessage(sender, "we are here!")
 
-            // //     }); 
+                }); 
 
 
 
