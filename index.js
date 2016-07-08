65f6783b928d98c3a1c7e4313eaf3483315c9b11
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-07 18:11:14 -0700

    message

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ app.post('/webhook/', function (req, res) {
 var token = "EAANGyeqRbP4BAL4qOjj2EgeiTCEEoNDg8OeuykOmTnHZC8P2VpEmVMKpAvCVLxF50p7ZARtahrYbMcvV14oH2VIOQDk5srjgQlQxKbEsZArbUZCZCUBkKaZA2IReylaHxY2Av0Be2exmqfjcZAo7RJZAdroNg1SAOsCceomp0y8pJgZDZD"
 
 
-String.function getUserName(sender) {
+function getUserName(sender) {
         var firstName
         var URL = "https://graph.facebook.com/v2.6/" + sender + "?fields=first_name&access_token=EAANGyeqRbP4BAL4qOjj2EgeiTCEEoNDg8OeuykOmTnHZC8P2VpEmVMKpAvCVLxF50p7ZARtahrYbMcvV14oH2VIOQDk5srjgQlQxKbEsZArbUZCZCUBkKaZA2IReylaHxY2Av0Be2exmqfjcZAo7RJZAdroNg1SAOsCceomp0y8pJgZDZD"
                  requestify.get(URL).then(function(response) {
@@ -296,6 +296,8 @@ String.function getUserName(sender) {
                     firstName = rep.first_name
 
                 });
+
+        sendTextMessage(sender, firstName)
         return firstName     
 
 
