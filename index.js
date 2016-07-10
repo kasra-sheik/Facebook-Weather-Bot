63c5e8e53932b96f7c4b60e5bb5740ed58521e04
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-10 16:47:19 -0700

    message

--- index.js
+++ index.js
@@ -335,15 +335,16 @@ function sunny(sender, location) {
         var rep = response.getBody();
 
         //sendTextMessage(sender, rep.weather[0].description)    
-        var weatherDescription = rep.weather[0].main
+        //var weatherDescription = rep.weather[0].main
+        sendTextMessage(sender, rep.weather[0].main)
 
-        if(weatherDescription.includes("Clear")) {
-            sendTextMessage(sender, "Yes, the sun is out.. shining with a temperature of " + rep.main.temp + " in " rep.name)
-        }
-        else{
-            sendTextMessage(sender, "Nope. Looks like there is " + weatherDescription)
+        // if(weatherDescription.includes("Clear")) {
+        //     sendTextMessage(sender, "Yes, the sun is out.. shining with a temperature of " + rep.main.temp + " in " rep.name)
+        // }
+        // else{
+        //     sendTextMessage(sender, "Nope. Looks like there is " + weatherDescription)
 
-        }    
+        // }    
 
     }); 
 
