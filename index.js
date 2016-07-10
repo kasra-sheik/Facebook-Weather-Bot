aa003d47da45dab53c8df4fcc45408283fefa0ce
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-10 16:16:49 -0700

    message

--- index.js
+++ index.js
@@ -84,6 +84,8 @@ app.post('/webhook/', function (req, res) {
                         if("intent" in rep.entities) {
                             intent = rep.entities.intent[0].value
                         }
+
+                        sendTextMessage(sender, "this is the intent: " + intent)
                         //grab weather
                         if(intent == "weather") {
                             if("location" in rep.entities) {
