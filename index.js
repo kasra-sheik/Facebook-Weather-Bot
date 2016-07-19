6ec97b17f5e14ac4b8399b0cd4f6de0f92bc59af
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-19 16:50:18 -0700

    message

--- index.js
+++ index.js
@@ -132,6 +132,7 @@ app.post('/webhook/', function (req, res) {
                                 var item = ""
                                 if("wit_item" in rep.entities) {
                                     item = rep.entities.wit_item[0].value
+                                    query = item 
                                 }
                                 else {
                                     sendTextMessage(sender, "this is where we crash..?") 
