6faf1ea5b0e58d6df04ae55711503118a15f57f5
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-19 16:42:43 -0700

    message

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.post('/webhook/', function (req, res) {
 
                             }
                         }
-                            else if("amount_of_money" in rep.entities) {
+                        if("amount_of_money" in rep.entities) {
                                 var item = ""
                                 if("wit_item" in rep.entities) {
                                     item = rep.entities.wit_item[0].value
