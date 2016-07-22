7db326a8cea09809a944c4031a2387ad418cc35c
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-22 15:03:43 -0700

    message

--- index.js
+++ index.js
@@ -452,6 +452,7 @@ function showCartItems(sender, id) {
     elementTest.shift()
     sendTextMessage(sender, elementTest.length)
  
+    if(elementTest.length > 0)  {
     messageData = {
         "attachment": {
             "type": "template",
@@ -476,7 +477,11 @@ function showCartItems(sender, id) {
             console.log('Error: ', response.body.error)
         }
     })
-
+ }
+ else {
+    var cartName = rep.items[0].cart.name
+    sendTextMessage(sender, "I didn't find any items in " + cartName ". You can add any item I can find for you or view other carts. ")
+ }
 
 
 
