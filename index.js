783565627f5bcf7529f497171e564dad69642b13
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-07-30 11:43:18 -0700

    message

--- index.js
+++ index.js
@@ -59,11 +59,12 @@ app.listen(app.get('port'), function() {
 
 
 app.post('/webhook/', function (req, res) {
-    console.log("we're here")
     messaging_events = req.body.entry[0].messaging
+
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
+        console.log(sender)
 
         var URL = "https://graph.facebook.com/v2.6/" + sender + "?fields=first_name,gender&access_token=EAANGyeqRbP4BAL4qOjj2EgeiTCEEoNDg8OeuykOmTnHZC8P2VpEmVMKpAvCVLxF50p7ZARtahrYbMcvV14oH2VIOQDk5srjgQlQxKbEsZArbUZCZCUBkKaZA2IReylaHxY2Av0Be2exmqfjcZAo7RJZAdroNg1SAOsCceomp0y8pJgZDZD"
                  requestify.get(URL).then(function(response) {
@@ -413,11 +414,7 @@ app.post('/webhook/', function (req, res) {
 })
 
 var token = "EAANGyeqRbP4BAL4qOjj2EgeiTCEEoNDg8OeuykOmTnHZC8P2VpEmVMKpAvCVLxF50p7ZARtahrYbMcvV14oH2VIOQDk5srjgQlQxKbEsZArbUZCZCUBkKaZA2IReylaHxY2Av0Be2exmqfjcZAo7RJZAdroNg1SAOsCceomp0y8pJgZDZD"
- if(n == 6) {
-    if(sender != null) {
-        sendTextMessage(sender, "happy saturday :-)")
-    }
- }
+
 
 function setSearchPreferences(sender) {
    messageData = {
