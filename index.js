0112a1e9854c29a6c5cab65e44c5c499e5168e4d
Kasra Sheik <Kasra@Kasras-MacBook-Pro.local>
2016-08-05 15:47:02 -0700

    message

--- index.js
+++ index.js
@@ -188,10 +188,8 @@ app.post('/webhook/', function (req, res) {
                                 return;
                               }
                                 if(content.hits.length > 0) {
-                                    if(someOneElse){sendTextMessage(sender, "I think I found some items your will like!")}
                                     sendTextMessage(sender, "I think I found some items you might like..")
                                     mavatarItemGenerator(sender, content, item, 0)
-                                    sendTextMessage()
                                 }
                                 else { 
                                     sendTextMessage(sender, "I'm sorry I couldn't find what you were looking for.. try broadening your search.")
